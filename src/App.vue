520899eb27b3ef46f9448784b96d75c4cb5d9ef4
冯然浩 <fengrh@lenovocloud.com>
2020-05-26 20:47:15 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,81 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header height="40px"></el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+                  default-active="2"
+                  class="el-menu-vertical-demo"
+                  @open="handleOpen"
+                  @close="handleClose">
+            <el-menu-item index="1">
+              <i class="el-icon-s-marketing"></i>
+              <span slot="title">盘符稳定性</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <i class="el-icon-s-data"></i>
+              <span slot="title">盘符BVT</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-s-marketing"></i>
+              <span slot="title">PC客户端稳定性</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-s-data"></i>
+              <span slot="title">PC客户端BVT</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
+<script>
+  export default {
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  html,body,#app,.el-container{
+    height: 100%;
+  }
 
-#nav {
-  padding: 30px;
-}
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 40px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .el-aside {
+    background-color: #FFFFFF;
+    color: #333;
+    text-align: start;
+    line-height: 200px;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
 </style>
